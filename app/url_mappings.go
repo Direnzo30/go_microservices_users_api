becfad6b8b0b532f02f0b87159907bfdaac924ce
Salvador Di Renzo <direnzosalvatore@gmail.com>
2020-11-20 17:25:32 -0400

    separate controllers paths

--- app/url_mappings.go
+++ app/url_mappings.go
@@ -1,15 +1,16 @@
 package app
 
 import (
-	"github.com/Direnzo30/go_microservices_users_api/controllers"
+	"github.com/Direnzo30/go_microservices_users_api/controllers/ping"
+	"github.com/Direnzo30/go_microservices_users_api/controllers/users"
 )
 
 func mapUrls() {
 	v1 := router.Group("/api/v1")
 	// Ping Controller Routes
-	v1.GET("/ping", controllers.Ping)
+	v1.GET("/ping", ping.Ping)
 	// Users Controller Routes
-	v1.GET("/users/:id", controllers.GetUser)
+	v1.GET("/users/:id", users.GetUser)
 	//v1.GET("/users/search", controllers.SearchUser)
-	v1.POST("/users", controllers.CreateUser)
+	v1.POST("/users", users.CreateUser)
 }
