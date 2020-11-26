76f77815c658fdaf3de7659979753dba555e4a9a
Salvador Di Renzo <direnzosalvatore@gmail.com>
2020-11-25 23:35:35 -0400

    fix time type for user

--- domain/users/user_dto.go
+++ domain/users/user_dto.go
@@ -1,14 +1,18 @@
 package users
 
-import "github.com/Direnzo30/go_microservices_users_api/utils/errors"
+import (
+	"time"
+
+	"github.com/Direnzo30/go_microservices_users_api/utils/errors"
+)
 
 // User struct handles the users entity
 type User struct {
-	ID        int64  `json:"id"`
-	FirstName string `json:"first_name"`
-	LastName  string `json:"last_name"`
-	Email     string `json:"email"`
-	CreatedAt string `json:"created_at"`
+	ID        int64     `json:"id"`
+	FirstName string    `json:"first_name"`
+	LastName  string    `json:"last_name"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"created_at"`
 }
 
 // Validate control if the user istance is valid
