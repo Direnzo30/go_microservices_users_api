672d5c5fd172c4cfef5577fd971d7bd8df261127
Salvador Di Renzo <direnzosalvatore@gmail.com>
2020-11-25 23:53:40 -0400

    load env file for development env, fix db connect

--- app/application.go
+++ app/application.go
@@ -1,8 +1,12 @@
 package app
 
 import (
+	"errors"
+	"os"
+
 	"github.com/Direnzo30/go_microservices_users_api/datasources/psql"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 // This will be controlling the app routing
@@ -12,10 +16,20 @@ var (
 
 // StartApplication runs the server
 func StartApplication() {
+	// Initialize custom ENV vars
+	setEnvironmentVars()
 	// Initialize connections
 	psql.InitHandlers()
 	// Sets Routes for controllers
 	mapUrls()
 	// Launch Server
 	router.Run(":3000")
+}
+
+func setEnvironmentVars() {
+	if os.Getenv("GO_ENVIRONMET") == "development" {
+		if err := godotenv.Load(".env"); err != nil {
+			panic(errors.New("UNABLE TO LOAD ENV VARIABLES"))
+		}
+	}
 }
--- datasources/psql/userdb/user_db.go
+++ datasources/psql/userdb/user_db.go
@@ -2,7 +2,6 @@ package userdb
 
 import (
 	"database/sql"
-	"fmt"
 	"os"
 
 	// required for psql connection
@@ -13,12 +12,7 @@ import (
 func InitUsersDB() *sql.DB {
 	var client *sql.DB
 	var err error
-	user := os.Getenv("DB_USER")
-	pssw := os.Getenv("DB_PSSW")
-	host := os.Getenv("DB_HOST")
-	port := os.Getenv("DB_PORT")
-	name := os.Getenv("DB_NAME")
-	dataSoruceName := fmt.Sprintf("user=%s password=%s host=%s port=%s dbname=%s sslmode=disable", user, pssw, host, port, name)
+	dataSoruceName := os.Getenv("DB_SOURCE_USERS")
 
 	client, err = sql.Open("postgres", dataSoruceName)
 	if err != nil {
