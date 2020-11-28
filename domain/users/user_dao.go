684eab72d4ce43597431bad722bb4fc2c1bc89d5
Salvador Di Renzo <direnzosalvatore@gmail.com>
2020-11-27 22:33:05 -0400

    add username, create insert - save

--- domain/users/user_dao.go
+++ domain/users/user_dao.go
@@ -1,6 +1,15 @@
 package users
 
-import "github.com/Direnzo30/go_microservices_users_api/utils/errors"
+import (
+	"time"
+
+	"github.com/Direnzo30/go_microservices_users_api/datasources/psql"
+	"github.com/Direnzo30/go_microservices_users_api/utils/errors"
+)
+
+const (
+	insertQuery = "INSERT INTO users(first_name, last_name, email, username, created_at) VALUES(?, ?, ?, ?, ?);"
+)
 
 // Get performs a find by id for users
 func (u *User) Get() *errors.RestError {
@@ -9,5 +18,23 @@ func (u *User) Get() *errors.RestError {
 
 // Save ensures to save the user in the database
 func (u *User) Save() *errors.RestError {
+	var err error
+	// Prepare insertion statement
+	statement, err := psql.Connections.Users.Prepare(insertQuery)
+	if err != nil {
+		return errors.InternalServerError(err.Error())
+	}
+	// Make sure to close statement
+	defer statement.Close()
+	insertResult, err := statement.Exec(u.FirstName, u.LastName, u.Email, u.Username, time.Now().UTC())
+	if err != nil {
+		return errors.InternalServerError(err.Error())
+	}
+	userID, err := insertResult.LastInsertId()
+	if err != nil {
+		return errors.InternalServerError(err.Error())
+	}
+	// Assgin ID created
+	u.ID = userID
 	return nil
 }
--- domain/users/user_dto.go
+++ domain/users/user_dto.go
@@ -12,6 +12,7 @@ type User struct {
 	FirstName string    `json:"first_name"`
 	LastName  string    `json:"last_name"`
 	Email     string    `json:"email"`
+	Username  string    `json:"username"`
 	CreatedAt time.Time `json:"created_at"`
 }
 
