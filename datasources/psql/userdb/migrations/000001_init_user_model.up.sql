eb597492cc8a9d1757b466962a68c15cf58a936f
Salvador Di Renzo <direnzosalvatore@gmail.com>
2020-12-01 22:35:19 -0400

    fix user migration

--- datasources/psql/userdb/migrations/000001_init_user_model.up.sql
+++ datasources/psql/userdb/migrations/000001_init_user_model.up.sql
@@ -6,7 +6,13 @@ CREATE TABLE users (
     last_name VARCHAR(60) NOT NULL,
     email VARCHAR NOT NULL UNIQUE,
     username VARCHAR NOT NULL UNIQUE,
-    created_at TIMESTAMP DEFAULT NOW()
+    password VARCHAR NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW()
 );
+
+CREATE UNIQUE INDEX idx_email ON users (email);
+CREATE UNIQUE INDEX idx_username ON users (username);
+
 
 COMMIT;
